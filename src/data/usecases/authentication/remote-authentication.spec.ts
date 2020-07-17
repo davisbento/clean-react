a6e539ae2cf8f3def0fcfb38792361f97efdb26d
davisbento <davisilva4222@gmail.com>
2020-07-17 16:42:36 -0300

    feat: authentication specs

---
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -0,0 +1,12 @@
+import { RemoteAuthentication } from './remote-authentication'
+import { HttpPostClientSpy } from '@/data/mock/mock-http-client'
+
+describe('RemoteAuthentication', () => {
+  test('Should call HttpClient with correct URL', async () => {
+    const url = 'any_url'
+    const httpPostClientSpy = new HttpPostClientSpy()
+    const sut = new RemoteAuthentication(url, httpPostClientSpy)
+    await sut.auth()
+    expect(httpPostClientSpy.url).toBe(url)
+  })
+})
